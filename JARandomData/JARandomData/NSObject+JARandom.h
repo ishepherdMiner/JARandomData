3ae44f55e114f8d6a8b9dbb7e1eddbae759bf141
Jason <iJason92@yahoo.com>
2017-02-05 22:47:09 +0800

    创建分类文件

---
+++ JARandomData/JARandomData/NSObject+JARandom.h
@@ -0,0 +1,15 @@
+//
+//  NSObject+JARandom.h
+//  JARandomData
+//
+//  Created by Jason on 05/02/2017.
+//  Copyright © 2017 Jason. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (JARandom)
+
+- (void)randomData;
+
+@end
---
+++ JARandomData/JARandomData/NSObject+JARandom.m
@@ -0,0 +1,16 @@
+//
+//  NSObject+JARandom.m
+//  JARandomData
+//
+//  Created by Jason on 05/02/2017.
+//  Copyright © 2017 Jason. All rights reserved.
+//
+
+#import "NSObject+JARandom.h"
+
+@implementation NSObject (JARandom)
+
+- (void)randomData {
+    NSLog(@"生成随机数据");
+}
+@end
